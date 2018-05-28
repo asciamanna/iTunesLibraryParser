a4323a2473bfd662e55a1133ce214874144eb622
Anthony Sciamanna <asciamanna@gmail.com>
2018-05-28 10:59:16 -0400

    Introduces a base parser object
    - Removes duplication between parsers

---
+++ iTunesLibraryParser/ParserBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ITunesLibraryParser {
+    internal abstract class ParserBase {
+        protected IEnumerable<XElement> ParseElements(string libraryContents) {
+            return from x in XDocument.Parse(libraryContents).Descendants("dict").Descendants(GetCollectionNodeName()).Descendants("dict")
+                where x.Descendants("key").Count() > 1
+                select x;
+        }
+
+        protected abstract string GetCollectionNodeName();
+    }
+}
