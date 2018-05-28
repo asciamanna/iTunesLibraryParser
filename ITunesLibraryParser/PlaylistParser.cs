359932016ce558cbaade5b3406d50f66ed152caf
Anthony Sciamanna <asciamanna@gmail.com>
2018-05-28 07:32:30 -0400

    Refactoring - introducing parser objects
    - Pulls parser logic out of library object and puts it in individual
      classes.

---
+++ ITunesLibraryParser/PlaylistParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ITunesLibraryParser {
+    public class PlaylistParser {
+        private readonly Dictionary<int, Track> trackLookup;
+
+        public PlaylistParser(IEnumerable<Track> tracks) {
+            trackLookup = tracks.ToDictionary(t => t.TrackId);
+        }
+        public IEnumerable<Playlist> ParsePlaylists(string libraryContents) {
+            return ParsePlaylistElements(libraryContents).Select(CreatePlaylist);
+        }
+        
+        public IEnumerable<XElement> ParsePlaylistElements(string libraryContents) {
+            return XDocument.Parse(libraryContents).Descendants("dict").Descendants("array")
+                .Descendants("dict").Where(node => node.Descendants("key").Count() > 1);
+        }
+
+        private Playlist CreatePlaylist(XElement playlistElement) {
+            return new Playlist {
+                PlaylistId = Int32.Parse(XElementParser.ParseStringValue(playlistElement, "Playlist ID")),
+                Name = XElementParser.ParseStringValue(playlistElement, "Name"),
+                Tracks = FindTracksInLibrary(playlistElement)
+            };
+        }
+
+        private IEnumerable<Track> FindTracksInLibrary(XElement playlistElement) {
+            var trackIdElements = playlistElement.Descendants("dict").Descendants("integer");
+            var trackIds = trackIdElements.Select(t => t.Value).Select(Int32.Parse);
+            return BuildTrackList(trackIds);
+        }
+
+        private List<Track> BuildTrackList(IEnumerable<int> trackIds) {
+            var playlistTracks = new List<Track>();
+            foreach (var trackId in trackIds) {
+                trackLookup.TryGetValue(trackId, out var matchingTrack);
+                if (matchingTrack != null)
+                    playlistTracks.Add(matchingTrack);
+            }
+            return playlistTracks;
+        }
+    }
+}
---
+++ ITunesLibraryParser/TrackParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ITunesLibraryParser {
+    public class TrackParser {
+        public IEnumerable<Track> ParseTracks(string libraryContents) {
+            return ParseTrackElements(libraryContents).Select(CreateTrack);
+        }
+
+        private IEnumerable<XElement> ParseTrackElements(string libraryContents) {
+            return from x in XDocument.Parse(libraryContents).Descendants("dict").Descendants("dict").Descendants("dict")
+                where x.Descendants("key").Count() > 1
+                select x;
+        }
+
+        private static Track CreateTrack(XElement trackElement) {
+            return new Track {
+                TrackId = Int32.Parse(XElementParser.ParseStringValue(trackElement, "Track ID")),
+                Name = XElementParser.ParseStringValue(trackElement, "Name"),
+                Artist = XElementParser.ParseStringValue(trackElement, "Artist"),
+                AlbumArtist = XElementParser.ParseStringValue(trackElement, "AlbumArtist"),
+                Composer = XElementParser.ParseStringValue(trackElement, "Composer"),
+                Album = XElementParser.ParseStringValue(trackElement, "Album"),
+                Genre = XElementParser.ParseStringValue(trackElement, "Genre"),
+                Kind = XElementParser.ParseStringValue(trackElement, "Kind"),
+                Size = XElementParser.ParseNullableLongValue(trackElement, "Size"),
+                PlayingTime = TimeConvert.MillisecondsToFormattedMinutesAndSeconds((XElementParser.ParseNullableLongValue(trackElement, "Total Time"))),
+                TrackNumber = XElementParser.ParseNullableIntValue(trackElement, "Track Number"),
+                Year = XElementParser.ParseNullableIntValue(trackElement, "Year"),
+                DateModified = XElementParser.ParseNullableDateValue(trackElement, "Date Modified"),
+                DateAdded = XElementParser.ParseNullableDateValue(trackElement, "Date Added"),
+                BitRate = XElementParser.ParseNullableIntValue(trackElement, "Bit Rate"),
+                SampleRate = XElementParser.ParseNullableIntValue(trackElement, "Sample Rate"),
+                PlayDate = XElementParser.ParseNullableDateValue(trackElement, "Play Date UTC"),
+                PlayCount = XElementParser.ParseNullableIntValue(trackElement, "Play Count"),
+                PartOfCompilation = XElementParser.ParseBoolean(trackElement, "Compilation"),
+            };
+        }
+    }
+}
